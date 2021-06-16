40488f04bc1b5e51c238e9c65f28690662c07da5
Antônio Falcão Jr <antonio.falcao@xpi.com.br>
2021-06-16 17:07:22 -0300

    Adding Entities and Value Objects in Domain project

---
+++ Dotnet6.EFCore6.Record.ValueObject.Domain/Entities/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Dotnet6.EFCore6.Record.ValueObject.Domain.Entities.Abstractions;
+using Dotnet6.EFCore6.Record.ValueObject.Domain.Exceptions;
+using Dotnet6.EFCore6.Record.ValueObject.Domain.ValueObjects;
+
+namespace Dotnet6.EFCore6.Record.ValueObject.Domain.Entities
+{
+    public class Person : Entity<Guid>
+    {
+        public Person(string name, int age) 
+            => (Name, Age) = (name, age);
+
+        public string Name { get; }
+        public int Age { get; }
+        public Address Address { get; private set; }
+
+        public void DefineAddress(Address address)
+        {
+            if (address is null) throw new BusinessException("Home address must be informed");
+            if(address.Equals(Address)) return;
+            Address = address;
+        }
+    }
+}
---
+++ Dotnet6.EFCore6.Record.ValueObject.Domain/ValueObjects/Address.cs
@@ -0,0 +1,16 @@
+﻿namespace Dotnet6.EFCore6.Record.ValueObject.Domain.ValueObjects
+{
+    public record Address : Abstractions.ValueObject
+    {
+        // Empty constructor in this case is required by EF Core,
+        // because has a complex type as a parameter in the default constructor.
+        private Address() { }
+
+        public Address(Street street, string zipCode)
+            => (Street, ZipCode) = (street, zipCode);
+
+        // Explicitly setter required for complex type by mapping engine.
+        public Street Street { get; private init; }
+        public string ZipCode { get; }
+    }
+}

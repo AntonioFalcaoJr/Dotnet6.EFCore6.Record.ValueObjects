037a2574c9cccee73d408da625d6b08e621540ed
Antônio Falcão Jr <antonio.falcao@xpi.com.br>
2021-06-16 17:24:39 -0300

    Moving repository project to src path

--- Dotnet6.EFCore6.Record.ValueObject.Repositories/IUserRepository.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-using Dotnet6.EFCore6.Record.ValueObject.Domain.Entities;
-using Dotnet6.EFCore6.Record.ValueObject.Repositories.Abstractions;
-
-namespace Dotnet6.EFCore6.Record.ValueObject.Repositories
-{
-    public interface IUserRepository : IRepository<Person, Guid> { }
-}

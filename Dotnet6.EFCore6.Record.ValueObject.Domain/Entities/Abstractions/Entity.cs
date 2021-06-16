7a95b15b16a8cd6adc8abeeb6bce6a2bc05cf9e4
Antônio Falcão Jr <antonio.falcao@xpi.com.br>
2021-06-16 17:24:54 -0300

    Moving Domain project to src path

--- Dotnet6.EFCore6.Record.ValueObject.Domain/Entities/Abstractions/Entity.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Dotnet6.EFCore6.Record.ValueObject.Domain.Entities.Abstractions
-{
-    public class Entity<TId>
-        where TId : struct
-    {
-        public TId Id { get; init; }
-    }
-}
--- Dotnet6.EFCore6.Record.ValueObject.Domain/ValueObjects/Abstractions/ValueObject.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Dotnet6.EFCore6.Record.ValueObject.Domain.ValueObjects.Abstractions
-{
-    public record ValueObject
-    {
-        // This implementation was kept only to specify the signature of its derivations,
-        // but it is no longer useful for it, since the comparison operator overrides are no longer necessary.
-    }
-}
